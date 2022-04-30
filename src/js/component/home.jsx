d2bb66446486471434aaf0fe88acd119f0ea938b
ilorlando10 <orlandolife@hotmail.com>
2022-04-30 15:04:01 +0000

    Finished Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [toDoInput, setToDoInput] = useState("");
+	const [taskArray, setTaskArray] = useState([]);
+
+	let toDoTasks = taskArray.map((task, index) => {
+		return (
+			<div className="list" key={task}>
+				{task}{" "}
+				<div className="button">
+					<button
+						className="deletebutton"
+						onClick={() => removeTask(index)}>
+						<i class="fa-solid fa-trash-can"></i>
+					</button>
+				</div>
+			</div>
+		);
+	});
+	let removeTask = (i) => {
+		let filteredNumber = taskArray.filter((task, index) => i != index);
+		setTaskArray(filteredNumber);
+	};
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="body">
+				<div>
+					<h1>To-Do List!!!</h1>
+				</div>
+				<div>
+					<input
+						type="text"
+						onChange={(e) => setToDoInput(e.target.value)}
+						value={toDoInput}
+						onKeyUp={(e) => {
+							if (e.key === "Enter") {
+								setTaskArray([...taskArray, toDoInput]);
+								setToDoInput("");
+							}
+						}}
+						placeholder="Add To-Do!!!"
+					/>
+
+					<ul>{toDoTasks}</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
